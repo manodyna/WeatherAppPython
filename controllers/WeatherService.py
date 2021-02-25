933b18658db9f68d2e816404f31fe8561f831160
manodyna <manodynak@gmail.com>
2021-02-25 17:08:42 +0530

    Initial commit

---
+++ controllers/WeatherService.py
@@ -0,0 +1,110 @@
+#This is the weather service class to call the current weather data API and retrive its contents
+import json
+import requests
+#package requests might have to installed seperatly in your local project.
+#PyCharm will handle it automatically if you use PyCharm.
+
+#bangalore has been passed as the city for testing only, the city selection functionality shoud be added during frontend dev
+#current data
+#temp refers to temperature
+#coodr refers to coordinate
+#resuts refers to the weather array
+#appid is the api key and is to be kept private
+def getWeatherArray():
+    __appid = "6c4a66dafb7d9f1fd2610e07e9396331"
+    url = "http://api.openweathermap.org/data/2.5/weather?q="+"bangalore"+"&appid="+__appid + "&units=metric"
+    response = requests.post(url)
+    response.raise_for_status()
+#
+    results = response.json()
+    return results
+
+def getCoord():
+    results = getWeatherArray()
+    return (results['coord'])
+
+def getWeatherMain():
+    results = getWeatherArray()
+    return (results['weather'][0]['main'])
+
+def getWeatherDescription():
+    results = getWeatherArray()
+    return (results['weather'][0]['description'])
+
+def getTemp():
+    results = getWeatherArray()
+    return (results['main']['temp'])
+
+def getFeelslike():
+    results = getWeatherArray()
+    return (results['main']['feels_like'])
+
+def getTempMin():
+    results = getWeatherArray()
+    return (results['main']['temp_min'])
+
+def getTempMax():
+    results = getWeatherArray()
+    return (results['main']['temp_max'])
+
+def getPressure():
+    results = getWeatherArray()
+    return (results['main']['pressure'])
+
+def getHumidity():
+    results = getWeatherArray()
+    return (results['main']['humidity'])
+
+def getLat():
+    results = getWeatherArray()
+    return (results['coord']['lat'])
+
+def getLon():
+    results = getWeatherArray()
+    return (results['coord']['lon'])
+
+lat = str(getLat())
+lon = str(getLon())
+
+def getForecastArray():
+    __appid = "6c4a66dafb7d9f1fd2610e07e9396331"
+    url = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=hourly,current,minutely&appid="+__appid+"&units=metric"
+    response = requests.post(url)
+    response.raise_for_status()
+
+    results = response.json()
+    return results()
+
+def getDayTemp():
+    results = getForecastArray()
+    dayTemp= []
+    for i in range (7):
+        dayTemp.append(results['daily'][i]['temp']['day'])
+    return dayTemp
+
+def getNightTemp():
+    results = getForecastArray()
+    nightTemp= []
+    for i in range (7):
+        nightTemp.append(results['daily'][i]['temp']['night'])
+    return nightTemp
+
+# def getNightTempFeelsLike():
+#     results = getForecastArray()
+#     nightTempFeelsLike = []
+#     for i in range (7):
+#         nightTempFeelsLike.append(results['daily'][i]['feels_like']['temp']['night'])
+#     return nightTempFeelsLike
+#
+# def getDayTempFeelsLike():
+#     results = getForecastArray()
+#     dayTempFeelsLike = []
+#     for i in range (7):
+#         dayTempFeelsLike.append(results['daily'][i]['feels_like']['temp']['day'])
+#     return dayTempFeelsLike
+
+# def getDescription():
+#     results = getForecastArray()
+#     description = []
+#     for i in range (7):
+#         description.append(results['current']['weather'][0]['description'])
---
+++ main.py
@@ -0,0 +1,31 @@
+#This is the main method only meant to call the finished methods
+#No functions are to be created here unless absolutely necessary
+#All assets(images, icons etc) go in the model package
+#All controllers and service classes are in the contollers package
+#All UI files go in the views package
+#No classes are to be created outside these packages in the project root
+#Camel case has been used throughout the project unless in json objects
+#Most method and variable names are self explanatory
+
+from controllers import WeatherService
+
+# creating objects
+weather = WeatherService.getWeatherArray()
+coord = WeatherService.getCoord()
+main = WeatherService.getWeatherMain()
+description = WeatherService.getWeatherDescription()
+temp = WeatherService.getTemp()
+feelsLike = WeatherService.getFeelslike()
+minTemp = WeatherService.getTempMin()
+maxTemp = WeatherService.getTempMax()
+humidity = WeatherService.getHumidity()
+pressure = WeatherService.getPressure()
+
+#forecast objects
+forecast = WeatherService.getForecastArray()
+dayTemp = WeatherService.getDayTemp()
+nightTemp = WeatherService.getNightTemp()
+# dayTempFeelsLike = WeatherService.getDayTempFeelsLike()
+# nightTempFeelsLike = WeatherService.getNightTempFeelsLike()
+# forecastDescription = WeatherService.getDescription()
+print(description)
