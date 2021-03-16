ae51c4287f06b8ee505d65f067a724b64a91721b
manodyna <manodynak@gmail.com>
2021-03-16 23:04:07 +0530

    final

--- controllers/WeatherService.py
+++ controllers/WeatherService.py
@@ -1,6 +1,8 @@
 #This is the weather service class to call the current weather data API and retrive its contents
 import json
 import requests
+
+#from MainView import *
 #package requests might have to installed seperatly in your local project.
 #PyCharm will handle it automatically if you use PyCharm.
 
@@ -10,57 +12,76 @@ import requests
 #coodr refers to coordinate
 #resuts refers to the weather array
 #appid is the api key and is to be kept private
-def getWeatherArray():
-    __appid = "6c4a66dafb7d9f1fd2610e07e9396331"
-    url = "http://api.openweathermap.org/data/2.5/weather?q="+"bangalore"+"&appid="+__appid + "&units=metric"
-    response = requests.post(url)
-    response.raise_for_status()
-#
-    results = response.json()
-    return results
+
+#city = "bangalore"
+
+
+
+class WeatherService():
+
+    def __init__(self,location):
+        self.location=location
+
+    def getWeatherArray(self):
+        __appid__ = "6c4a66dafb7d9f1fd2610e07e9396331"
+        url = "http://api.openweathermap.org/data/2.5/weather?q="+self.location+"&appid="+__appid__ + "&units=metric"
+        response = requests.post(url)
+        response.raise_for_status()
+
+        results = response.json()
+        return results
+
+# from views.MainView import MainView
+# var = MainView.city
+
+
+
+weatherServiceArray = WeatherService("london")
+weatherArray = weatherServiceArray.getWeatherArray()
 
 def getCoord():
-    results = getWeatherArray()
-    return (results['coord'])
+    results = weatherArray
+    coord = results['coord']
+    return coord
 
 def getWeatherMain():
-    results = getWeatherArray()
+    results = weatherArray
     return (results['weather'][0]['main'])
 
 def getWeatherDescription():
-    results = getWeatherArray()
+    results = weatherArray
     return (results['weather'][0]['description'])
 
 def getTemp():
-    results = getWeatherArray()
+    results = weatherArray
     return (results['main']['temp'])
 
 def getFeelslike():
-    results = getWeatherArray()
+    results = weatherArray
     return (results['main']['feels_like'])
 
 def getTempMin():
-    results = getWeatherArray()
+    results = weatherArray
     return (results['main']['temp_min'])
 
 def getTempMax():
-    results = getWeatherArray()
+    results = weatherArray
     return (results['main']['temp_max'])
 
 def getPressure():
-    results = getWeatherArray()
+    results = weatherArray
     return (results['main']['pressure'])
 
 def getHumidity():
-    results = getWeatherArray()
+    results = weatherArray
     return (results['main']['humidity'])
 
 def getLat():
-    results = getWeatherArray()
+    results = weatherArray
     return (results['coord']['lat'])
 
 def getLon():
-    results = getWeatherArray()
+    results = weatherArray
     return (results['coord']['lon'])
 
 lat = str(getLat())
@@ -89,22 +110,22 @@ def getNightTemp():
         nightTemp.append(results['daily'][i]['temp']['night'])
     return nightTemp
 
-# def getNightTempFeelsLike():
-#     results = getForecastArray()
-#     nightTempFeelsLike = []
-#     for i in range (7):
-#         nightTempFeelsLike.append(results['daily'][i]['feels_like']['temp']['night'])
-#     return nightTempFeelsLike
-#
-# def getDayTempFeelsLike():
-#     results = getForecastArray()
-#     dayTempFeelsLike = []
-#     for i in range (7):
-#         dayTempFeelsLike.append(results['daily'][i]['feels_like']['temp']['day'])
-#     return dayTempFeelsLike
+    # def getNightTempFeelsLike():
+    #     results = getForecastArray()
+    #     nightTempFeelsLike = []
+    #     for i in range (7):
+    #         nightTempFeelsLike.append(results['daily'][i]['feels_like']['temp']['night'])
+    #     return nightTempFeelsLike
+    #
+    # def getDayTempFeelsLike():
+    #     results = getForecastArray()
+    #     dayTempFeelsLike = []
+    #     for i in range (7):
+    #         dayTempFeelsLike.append(results['daily'][i]['feels_like']['temp']['day'])
+    #     return dayTempFeelsLike
 
-# def getDescription():
-#     results = getForecastArray()
-#     description = []
-#     for i in range (7):
-#         description.append(results['current']['weather'][0]['description'])
+    # def getDescription():
+    #     results = getForecastArray()
+    #     description = []
+    #     for i in range (7):
+    #         description.append(results['current']['weather'][0]['description'])
